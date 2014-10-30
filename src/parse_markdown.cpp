3b59b996f71177188b3e5487ac416ebc16e3bb94
WebTogz <antonin.carette@gmail.com>
2014-10-30 20:26:52 +0100

    parse_markdown.cpp: Search and replace bold and italic words

--- src/parse_markdown.cpp
+++ src/parse_markdown.cpp
@@ -17,7 +17,7 @@ using namespace std;
 /**
  * Regex
  */
-regex italic_regex("[a-zA-Z0-9 ]\\*([a-zA-Z0-9]*)\\*[a-zA-Z0-9 ]");
+regex italic_regex("[<>a-zA-Z0-9 ]*\\*([a-zA-Z0-9 ]*)\\*[<>a-zA-Z0-9 ]*");
 regex bold_regex("\\*\\*(.*)\\*\\*");
 regex list_regex("\\*\t(.*)");
 
@@ -33,18 +33,53 @@ bool entry = false;
 bool italic = false;
 bool bold = false;
 
+void str_replace( string &s, string &search, string &replace );
+
 string parseline(string& line) {
 
-	string stringToReturn;
+	string stringToReturn = line;
+
+	string toReplace;
+
+	string search;
 
 	smatch match;
 
-	if (regex_search(line, match, bold_regex)) {
-		stringToReturn += "<b>";
-		stringToReturn += match[1];
-		stringToReturn += "</b>";
+	while (regex_search(stringToReturn, match, bold_regex) || regex_search(stringToReturn, match, italic_regex)) {
+		if (regex_search(stringToReturn, match, bold_regex)) {
+			toReplace += "<b>";
+			toReplace += match[1];
+			toReplace += "</b>";
+			search += "**";
+			search += match[1];
+			search += "**";
+			str_replace(stringToReturn, search, toReplace);
+		}
+		toReplace = "";
+		search = "";
+		if (regex_search(stringToReturn, match, italic_regex)) {
+			toReplace += "<i>";
+			toReplace += match[1];
+			toReplace += "</i>";
+			search += "*";
+			search += match[1];
+			search += "*";
+			str_replace(stringToReturn, search, toReplace);
+		}
 	}
 
 	return stringToReturn;
 
+}
+
+void str_replace( string &s, string &search, string &replace ) {
+	for( size_t pos = 0; ; pos += replace.length() )
+	{
+		pos = s.find( search, pos );
+		if( pos == string::npos )
+			break;
+
+		s.erase( pos, search.length() );
+		s.insert( pos, replace );
+	}
 }
