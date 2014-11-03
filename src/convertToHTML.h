b81d47fe99b1547a5eb9e41668c5e69329f06ef8
WebTogz <antonin.carette@gmail.com>
2014-11-03 22:16:10 +0100

    convertToHTML.h: Verification functions returns bool now

--- src/convertToHTML.h
+++ src/convertToHTML.h
@@ -16,21 +16,21 @@ using namespace std;
 class convertToHTML {
 
 public:
-	void verification_bold(string& actualString);
+	bool verification_bold(string& actualString);
 
-	void verification_italic(string& actualString);
+	bool verification_italic(string& actualString);
 
-	void verification_url(string& actualString);
+	bool verification_url(string& actualString);
 
-	void verification_img(string& actualString);
+	bool verification_img(string& actualString);
 
-	void verification_non_ordonate_list(string& actualString);
+	bool verification_non_ordonate_list(string& actualString);
 
-	void verification_ordonate_list(string& actualString);
+	bool verification_ordonate_list(string& actualString);
 
-	void verification_headers(string& actualString);
+	bool verification_headers(string& actualString);
 
-	void verification_title(string& actualString);
+	bool verification_title(string& actualString);
 
 	string parseline(string& line);
 
